fafe32d49a8c193eb6ac554ba35da70e0c03749c
zhengyidong <zyidong.zheng@gmail.com>
2016-03-05 23:45:59 +0800

    combination added

---
+++ main/combination.cpp
@@ -0,0 +1,12 @@
+/* calculate C(n, m), number of k-combinations from a given set S of n elements. */
+/* time  complexity: O(m) */
+/* space complexity: O(1) */
+/* usage             :just use it. */
+int C(int n, int m){
+    int ans = 1;
+    for(int i=0; i<m; ++i){
+      ans *= n - i;
+      ans /= i + 1;
+    }
+    return ans;
+}
